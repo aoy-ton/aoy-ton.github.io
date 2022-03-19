02ddb01a8c3af3d643f7006fe9239dbbc84dbaba
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2022-03-19 20:39:29 +0700

    Update web

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2019, 11, 25, 10)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2022, 05, 13, 9)}); // year, month, date, hour
         });
 });		
 
